35a0a088bd9633612d0b9321b9dc51aa573cfa58
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2025-02-04 12:36:23 +0100

    feat: sane defaults

--- src/init/config.rs
+++ src/init/config.rs
@@ -1,16 +1,40 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Default)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
-    copy_files: bool,
     paths: Vec<SanityPaths>,
 }
 
-#[derive(Debug, Deserialize, Serialize, Default)]
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            paths: sanity_example_paths(),
+        }
+    }
+}
+
+fn sanity_example_paths() -> Vec<SanityPaths> {
+    vec![ 
+        SanityPaths {
+            path: "example/somefile.csv".to_string(),
+            copy_files: false,
+            tests: vec![Sanity::FileHash],
+        },
+        SanityPaths {
+            path: "otherexample/otherfile.csv".to_string(),
+            copy_files: false,
+            tests: vec![Sanity::FileHash],
+        }
+    ]
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 struct SanityPaths {
     path: String,
+    copy_files: bool,
     tests: Vec<Sanity>,
 }
+
 
 #[derive(Debug, Deserialize, Serialize, Default)]
 enum Sanity {
